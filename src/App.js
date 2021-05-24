bd9329cbadbdafb7602eb8025da87f4fa6c4dae2
Mrinmoy Sarkar <msarkar07@live.in>
2021-05-24 08:25:51 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
-  return (
+    // const [isActive, setActive] = useState(false);
+    const [number, setNumber] = useState(1);
+
+
+    const toggleClass = (n) => {
+        // setActive(!isActive);
+        setNumber(n);
+    };
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="tabs">
+            <button className={ (number ===1 ? 'btn-active': 'btn')}
+                    onClick={() => toggleClass(1)}
+                    disabled={number ===1 }
+           >
+                Section title 1
+            </button>
+            <button className={ (number ===2 ? 'btn-active': 'btn')}
+                    onClick={() => toggleClass(2)}
+                    disabled={number ===2 } >
+                Section title 2
+            </button>
+            <button className={ (number ===3 ? 'btn-active': 'btn')}
+                    onClick={() => toggleClass(3)}
+                    disabled={number ===3 }>
+                Section title 3
+            </button>
+            <div className="view">
+                <ComponentB title ={number} />
+            </div>
+        </div>
     </div>
   );
 }
 
 export default App;
+
+const ComponentB = (prop) => (<h1>Content of section {prop.title}</h1>);
+
+
+
+
+
+
+
+//     <MyTabsComponent>
+//     <div title={"Section title 1"}>Content of section 1</div>
+// <div title={"Section title 2"}>Content of section 2</div>
+// </MyTabsComponent>
+//
+// The above example should render this structure
+// <div class="tabs">
+// <button class="btn-active">Section title 1</button>
+// <button class="btn">Section title 2</button>
+// <div class="view">
+// Content of section 1
+// </div>
+// </div>
